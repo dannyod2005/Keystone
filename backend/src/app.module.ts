9717946d71901aa420f25ef91e8b77b5f59d1cc0
Danny O'Donnell <dxo326@student.bham.ac.uk>
2026-07-30 14:39:10 +0700

    Add ConfigModule and .env structure ahead of database setup (#5)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CoursesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
